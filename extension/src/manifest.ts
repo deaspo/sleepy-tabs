b65526a552cf92aeb1c0d5fcec0ec927f68321a3
Polycarp <deaspo@users.noreply.github.com>
2025-12-09 17:32:18 +0100

    feat: add 48x48 icon to manifest and update icon references

--- extension/src/manifest.ts
+++ extension/src/manifest.ts
@@ -52,6 +52,7 @@ const manifest = defineManifest({
   icons: {
     '16': 'public/icon-16.png',
     '32': 'public/icon-32.png',
+    '48': 'public/icon-48.png',
     '128': 'public/icon-128.png'
   }
 });
